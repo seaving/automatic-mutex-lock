36e08b2b76395ca4dcada900bd40e8b425a9945b
caixiwen <31427227@qq.com>
2019-07-17 09:01:20 +0800

    详见readme

--- src/mutex.c
+++ src/mutex.c
@@ -2,6 +2,13 @@
 #include "log_trace.h"
 #include "mutex.h"
 
+/*
+* 函数: mutex_create
+* 功能: 动态创建锁
+* 参数: 无
+* 返回: mutex_t *
+* 说明: 
+*/
 mutex_t *mutex_create()
 {
 	mutex_t *mutex = calloc(sizeof(mutex_t), 1);
@@ -15,6 +22,14 @@ mutex_t *mutex_create()
 	return mutex;
 }
 
+/*
+* 函数: mutex_destroy
+* 功能: 销毁创建的锁
+* 参数: mutex			互斥锁
+* 返回: bool
+*		- false		失败
+* 说明: 
+*/
 bool mutex_destroy(mutex_t *mutex)
 {
 	if (mutex == NULL)
@@ -25,6 +40,47 @@ bool mutex_destroy(mutex_t *mutex)
 	pthread_mutex_destroy(&mutex->lock);
 	memset(mutex, 0, sizeof(mutex_t));
 	free(mutex);
+	return true;
+}
+
+/*
+* 函数: spinlock_create
+* 功能: 动态创建自旋锁
+* 参数: 无
+* 返回: mutex_t *
+* 说明: 
+*/
+spinlock_t *spinlock_create()
+{
+	spinlock_t *spinlock = calloc(sizeof(spinlock_t), 1);
+	if (spinlock == NULL)
+	{
+		LOG_DEBUG_PERROR("calloc error !\n");
+		return NULL;
+	}
+
+	pthread_spin_init(&spinlock->lock, PTHREAD_PROCESS_PRIVATE);
+	return spinlock;
+}
+
+/*
+* 函数: spinlock_destroy
+* 功能: 销毁创建的锁
+* 参数: spinlock		自旋锁
+* 返回: bool
+*		- false		失败
+* 说明: 
+*/
+bool spinlock_destroy(spinlock_t *spinlock)
+{
+	if (spinlock == NULL)
+	{
+		return false;
+	}
+
+	pthread_spin_destroy(&spinlock->lock);
+	memset(spinlock, 0, sizeof(spinlock_t));
+	free(spinlock);
 	return true;
 }
 
--- src/mutex.h
+++ src/mutex.h
@@ -14,6 +14,17 @@ typedef struct __mutex__
 	bool codeBlock_noReturn;	//代码块中是否return标记
 } mutex_t;
 
+typedef struct __spinlock__
+{
+	pthread_spinlock_t lock;	//自旋锁
+	pid_t	pid;				//持有锁的进程ID
+	pthread_t thread_id;		//持有锁的线程ID
+	char *fun_name;				//持有锁的函数
+	int fun_line;				//持有锁的函数行号
+	bool locked;				//是否上锁标记
+	bool codeBlock_noReturn;	//代码块中是否return标记
+} spinlock_t;
+
 /*
 * 宏: MUTEX_INITIALIZER
 * 功能: 静态初始化 
@@ -30,112 +41,116 @@ typedef struct __mutex__
 }
 
 /*
-* 函数: mutex_sync_return_type_not_void
+* 函数: _lock_sync_return_type_not_void
 * 功能: 代码块同步
-* 参数: returnType		调用该宏的函数返回值类型
-*		_pMutex			mutex类型互斥锁
+* 参数: _lock_fun			上锁函数
+*		_unlock_fun		解锁函数
+*		returnType		调用该宏的函数返回值类型
+*		_pLock			锁
 *		_codeBlock		代码块
 *		fun				函数名
 *		line			行数
 * 返回: 无
 * 说明: 适用于函数返回值类型不是void
 */
-#define __mutex_sync_return_type_not_void(returnType, _pMutex, _codeBlock, fun, line)	\
+#define __lock_sync_return_type_not_void(_lock_fun, _unlock_fun, returnType, _pLock, _codeBlock, fun, line)	\
 	do { \
-		/*const char *__mutex_sync_fun_name = fun;*/ \
-		/*const int __mutex_sync_fun_line = line;*/ \
-		returnType _mutex_sync_##fun##line##_ret;\
-		if ((_pMutex)->locked == true \
-			&& pthread_self() == (_pMutex)->thread_id \
-			&& getpid() == (_pMutex)->pid) \
+		/*const char *__lock_sync_fun_name = fun;*/ \
+		/*const int __lock_sync_fun_line = line;*/ \
+		returnType _lock_sync_##fun##line##_ret;\
+		if ((_pLock)->locked == true \
+			&& pthread_self() == (_pLock)->thread_id \
+			&& getpid() == (_pLock)->pid) \
 		{ \
 			LOG_DEBUG_TRACE("!!!!!! WARN DeadLock (Holding lock fun: %s, line: %d) !!!!!!\n", \
-				(_pMutex)->fun_name, (_pMutex)->fun_line); \
+				(_pLock)->fun_name, (_pLock)->fun_line); \
 		} \
 		/*LOG_DEBUG_TRACE("pid=%lu, threadid=%lu, locked=%d\n", */ \
-			/*(_pMutex)->pid, (_pMutex)->thread_id, (_pMutex)->locked); */ \
-		pthread_mutex_lock(&((_pMutex)->lock)); \
-		(_pMutex)->pid = getpid(); \
-		(_pMutex)->thread_id = pthread_self(); \
-		(_pMutex)->locked = true; \
-		(_pMutex)->fun_name = (char *) fun; \
-		(_pMutex)->fun_line = (int) line; \
+			/*(_pLock)->pid, (_pLock)->thread_id, (_pLock)->locked); */ \
+		_lock_fun(&((_pLock)->lock)); \
+		(_pLock)->pid = getpid(); \
+		(_pLock)->thread_id = pthread_self(); \
+		(_pLock)->locked = true; \
+		(_pLock)->fun_name = (char *) fun; \
+		(_pLock)->fun_line = (int) line; \
 		\
-		returnType _mutex_sync_##fun##line() { \
+		returnType _lock_sync_##fun##line() { \
 			/*LOG_DEBUG_TRACE("<<< source fun: %s, source line: %d >>>\n",*/ \
-				/*	__mutex_sync_fun_name, __mutex_sync_fun_line);*/ \
-			(_pMutex)->codeBlock_noReturn = false; \
+				/*	__lock_sync_fun_name, __lock_sync_fun_line);*/ \
+			(_pLock)->codeBlock_noReturn = false; \
 			_codeBlock \
-			(_pMutex)->codeBlock_noReturn = true; \
+			(_pLock)->codeBlock_noReturn = true; \
 			return (returnType) 0; \
 		} \
 		/*LOG_DEBUG_TRACE("execut code block function >>> \n");*/ \
-		_mutex_sync_##fun##line##_ret = _mutex_sync_##fun##line();\
+		_lock_sync_##fun##line##_ret = _lock_sync_##fun##line();\
 		\
-		(_pMutex)->pid = 0; \
-		(_pMutex)->thread_id = 0; \
-		(_pMutex)->locked = false; \
-		(_pMutex)->fun_name = ""; \
-		(_pMutex)->fun_line = 0; \
-		pthread_mutex_unlock(&((_pMutex)->lock)); \
-		if ((_pMutex)->codeBlock_noReturn == false) \
+		(_pLock)->pid = 0; \
+		(_pLock)->thread_id = 0; \
+		(_pLock)->locked = false; \
+		(_pLock)->fun_name = ""; \
+		(_pLock)->fun_line = 0; \
+		_unlock_fun(&((_pLock)->lock)); \
+		if ((_pLock)->codeBlock_noReturn == false) \
 		{ \
 			/*LOG_DEBUG_TRACE("return ==> \n");*/ \
-			return _mutex_sync_##fun##line##_ret;\
+			return _lock_sync_##fun##line##_ret;\
 		} \
-	} while (0);
+	} while (0)
 
 /*
-* 函数: mutex_sync_return_type_is_void
+* 函数: __lock_sync_return_type_is_void
 * 功能: 代码块同步
-* 参数: returnType		调用该宏的函数返回值类型
-*		_pMutex 		mutex类型互斥锁
+* 参数: _lock_fun			上锁函数
+*		_unlock_fun		解锁函数
+*		returnType		调用该宏的函数返回值类型
+*		_pLock 			锁
 *		_codeBlock		代码块
 *		fun				函数名
 *		line			行数
 * 返回: 无
 * 说明: 适用于函数返回值类型void
 */
-#define __mutex_sync_return_type_is_void(/*returnType, */_pMutex, _codeBlock, fun, line)	\
+#define __lock_sync_return_type_is_void(_lock_fun, _unlock_fun, /*returnType, */_pLock, _codeBlock, fun, line)	\
 	do { \
-		/*const char *__mutex_sync_fun_name = fun;*/ \
-		/*const int __mutex_sync_fun_line = line;*/ \
-		if ((_pMutex)->locked == true \
-			&& pthread_self() == (_pMutex)->thread_id \
-			&& getpid() == (_pMutex)->pid) \
+		/*const char *__lock_sync_fun_name = fun;*/ \
+		/*const int __lock_sync_fun_line = line;*/ \
+		if ((_pLock)->locked == true \
+			&& pthread_self() == (_pLock)->thread_id \
+			&& getpid() == (_pLock)->pid) \
 		{ \
 			LOG_DEBUG_TRACE("!!!!!! WARN DeadLock (Holding lock fun: %s, line: %d) !!!!!!\n", \
-				(_pMutex)->fun_name, (_pMutex)->fun_line); \
+				(_pLock)->fun_name, (_pLock)->fun_line); \
 		} \
-		pthread_mutex_lock(&((_pMutex)->lock)); \
-		(_pMutex)->pid = getpid(); \
-		(_pMutex)->thread_id = pthread_self(); \
-		(_pMutex)->locked = true; \
-		(_pMutex)->fun_name = (char *)fun; \
-		(_pMutex)->fun_line = (int) line; \
+		_lock_fun(&((_pLock)->lock)); \
+		(_pLock)->pid = getpid(); \
+		(_pLock)->thread_id = pthread_self(); \
+		(_pLock)->locked = true; \
+		(_pLock)->fun_name = (char *)fun; \
+		(_pLock)->fun_line = (int) line; \
 		\
-		void _mutex_sync_##fun##line() { \
+		void _lock_sync_##fun##line() { \
 			/*LOG_DEBUG_TRACE("<<< source fun: %s, source line: %d >>>\n",*/ \
-					/*__mutex_sync_fun_name, __mutex_sync_fun_line);*/ \
-			(_pMutex)->codeBlock_noReturn = false; \
+					/*__lock_sync_fun_name, __lock_sync_fun_line);*/ \
+			(_pLock)->codeBlock_noReturn = false; \
 			_codeBlock \
-			(_pMutex)->codeBlock_noReturn = true; \
+			(_pLock)->codeBlock_noReturn = true; \
 		} \
 		/*LOG_DEBUG_TRACE("execut code block function >>> \n");*/ \
-		_mutex_sync_##fun##line();\
+		_lock_sync_##fun##line();\
 		\
-		(_pMutex)->pid = 0; \
-		(_pMutex)->thread_id = 0; \
-		(_pMutex)->locked = false; \
-		(_pMutex)->fun_name = ""; \
-		(_pMutex)->fun_line = 0; \
-		pthread_mutex_unlock(&((_pMutex)->lock)); \
-		if ((_pMutex)->codeBlock_noReturn == false) \
+		(_pLock)->pid = 0; \
+		(_pLock)->thread_id = 0; \
+		(_pLock)->locked = false; \
+		(_pLock)->fun_name = ""; \
+		(_pLock)->fun_line = 0; \
+		_unlock_fun(&((_pLock)->lock)); \
+		if ((_pLock)->codeBlock_noReturn == false) \
 		{ \
 			/*LOG_DEBUG_TRACE("return ==> \n");*/ \
 			return; \
 		} \
-	} while (0);
+	} while (0)
 
 /*
 * 函数: mutex_sync_return_type_not_void
@@ -147,7 +162,8 @@ typedef struct __mutex__
 * 说明: 适用于函数返回值类型不是void
 */
 #define mutex_sync_return_type_not_void(returnType, _pMutex, _codeBlock) \
-	__mutex_sync_return_type_not_void(returnType, _pMutex, _codeBlock, __FUNCTION__, __LINE__)
+	__lock_sync_return_type_not_void(pthread_mutex_lock, pthread_mutex_unlock, \
+			returnType, _pMutex, _codeBlock, __FUNCTION__, __LINE__)
 
 /*
 * 函数: mutex_sync_return_type_not_void
@@ -159,13 +175,72 @@ typedef struct __mutex__
 * 说明: 适用于函数返回值类型不是void
 */
 #define mutex_sync_return_type_is_void(/*returnType, */_pMutex, _codeBlock) \
-	__mutex_sync_return_type_is_void(/*returnType, */_pMutex, _codeBlock, __FUNCTION__, __LINE__)
+	__lock_sync_return_type_is_void(pthread_mutex_lock, pthread_mutex_unlock, \
+			/*returnType, */_pMutex, _codeBlock, __FUNCTION__, __LINE__)
 
-//动态初始化
+/*
+* 函数: spinlock_sync_return_type_not_void
+* 功能: 代码块同步
+* 参数: returnType		调用该宏的函数返回值类型
+*		_pSpinLock 		spinlock类型互斥锁
+*		_codeBlock		代码块
+* 返回: 无
+* 说明: 适用于函数返回值类型不是void
+*/
+#define spinlock_sync_return_type_not_void(returnType, _pSpinLock, _codeBlock) \
+	__lock_sync_return_type_not_void(pthread_mutex_lock, pthread_mutex_unlock, \
+			returnType, _pSpinLock, _codeBlock, __FUNCTION__, __LINE__)
+
+/*
+* 函数: spinlock_sync_return_type_is_void
+* 功能: 代码块同步
+* 参数: returnType		调用该宏的函数返回值类型
+*		_pSpinLock 		spinlock类型互斥锁
+*		_codeBlock		代码块
+* 返回: 无
+* 说明: 适用于函数返回值类型不是void
+*/
+#define spinlock_sync_return_type_is_void(/*returnType, */_pSpinLock, _codeBlock) \
+	__lock_sync_return_type_is_void(pthread_spin_lock, pthread_spin_unlock, \
+			/*returnType, */_pSpinLock, _codeBlock, __FUNCTION__, __LINE__)
+
+/*
+* 函数: mutex_create
+* 功能: 动态创建锁
+* 参数: 无
+* 返回: mutex_t *
+* 说明: 
+*/
 mutex_t *mutex_create();
 
-//释放锁资源
+/*
+* 函数: mutex_destroy
+* 功能: 销毁创建的锁
+* 参数: mutex			互斥锁
+* 返回: bool
+*		- false		失败
+* 说明: 
+*/
 bool mutex_destroy(mutex_t *mutex);
+
+/*
+* 函数: spinlock_create
+* 功能: 动态创建自旋锁
+* 参数: 无
+* 返回: mutex_t *
+* 说明: 
+*/
+spinlock_t *spinlock_create();
+
+/*
+* 函数: spinlock_destroy
+* 功能: 销毁创建的锁
+* 参数: spinlock		自旋锁
+* 返回: bool
+*		- false		失败
+* 说明: 
+*/
+bool spinlock_destroy(spinlock_t *spinlock);
 
 #endif
 
